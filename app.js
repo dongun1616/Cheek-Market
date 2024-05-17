54d3b42b747527a7c5eab4c8de5a58b80fad8c13
dongun0116 <dongun16@naver.com>
2024-05-17 21:43:02 +0900

    2024.05.17-1st-test-start

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-const test = 0;
-const test2 = 0;
-const test3 = 0;
-const test4 = 0;
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', (req, res) => {
+    res.send('This is the home page') //home.ejs로 전송
+})
+
+app.listen(3000, () => {
+    console.log('Serving on port 3000')
+})
