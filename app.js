b08f38ab0fe0c37aacf1e177916bdf4104cc7656
dongun0116 <dongun16@naver.com>
2024-07-03 19:32:57 +0900

    2024.07.03-cheek_market_test

--- app.js
+++ app.js
@@ -6,10 +6,9 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res) => {
-    res.send('This is the home page') //home.ejs로 전송
+    res.render('home') //home.ejs로 전송
 })
 
 app.listen(3000, () => {
-
     console.log('Serving on port 3000')
 })
