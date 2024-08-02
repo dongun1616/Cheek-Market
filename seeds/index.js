cbe92ef81926a4630e7608a18cc4b0bbe16f0475
dongun0116 <dongun16@naver.com>
2024-08-02 23:29:53 +0900

    2024.08.02-1st-index,new-styling-bootstrap5

--- seeds/index.js
+++ seeds/index.js
@@ -21,7 +21,7 @@ const seedDb = async () => { //랜덤하게 50개의 제품 및 위치 배정하
         const prod = new Product({
             title: `${sample(products)}`,
             location: `${sample(places)}`,
-            image: 'https://source.unsplash.com/random/300x300',
+            image: 'https://images.unsplash.com/photo-1523275335684-37898b6baf30?q=80&w=1099&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D/800x600',
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit Illo iure blanditiis rem nulla assumenda aperiam similique nostrum tempore dolore sequi suscipit provident reiciendis quasi doloremque sit id ut temporibus corrupti',
             price: rndPrice
         })
