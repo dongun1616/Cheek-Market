362fce96fbc7ac36e9cae59842e80ca940f3b381
dongun0116 <dongun16@naver.com>
2024-12-30 23:40:36 +0900

    test66

--- routes/products.js
+++ routes/products.js
@@ -10,7 +10,6 @@ const upload = multer({ storage })
 
 const ExpressError = require('../utils/ExpressError');
 
-
 // JOI 제품 유효성 검사 함수
 const validateProduct = (req, res, next) => {
     const { error } = productSchema.validate(req.body);
